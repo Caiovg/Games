23ada9001e4767b3ffa90e379d87cb842d708a87
unknown <caioviniciusg.1000@gmail.com>
2021-09-08 10:52:07 -0300

    Criação do metodo de logar

--- src/main/java/com/caio/games/services/UsuarioService.java
+++ src/main/java/com/caio/games/services/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.caio.games.services;
 
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.caio.games.services.exception.DataIntegratyViolationException;
 import com.caio.games.services.exception.ObjectNotFoundException;
+import com.caio.games.model.dtos.UserLogin;
 import com.caio.games.model.Usuario;
 import com.caio.games.repository.UsuarioRepository;
 
@@ -50,4 +53,33 @@ public class UsuarioService {
 		}));
 	}
 	
+	/*
+	 * Metodo de login 
+	 */
+	public Optional<?> logar(Optional<UserLogin> user){
+		//Verifica o email ou no meu caso o user
+		return Optional.ofNullable(repository.findByEmail(user.get().getEmail()).map(usuarioExistente -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+			//verifica as senhas 
+			if(encoder.matches(user.get().getSenha(), usuarioExistente.getSenha())) {
+					
+					String auth = user.get().getEmail() + ":" + user.get().getSenha();
+					byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+					String authHeader = "Basic " + new String(encodedAuth);
+					
+					user.get().setToken(authHeader);
+					user.get().setId(usuarioExistente.getIdUsuario());
+					user.get().setNome(usuarioExistente.getNome());
+					user.get().setSenha(usuarioExistente.getEmail());
+					
+					return Optional.ofNullable(user);
+			}else {
+				return Optional.empty().orElseThrow(() -> new ObjectNotFoundException("Senha Incorreta")); //Senha esteja incorreta
+			}
+			
+		}).orElseGet(() -> {
+			return Optional.empty().orElseThrow(() -> new ObjectNotFoundException("Usuario não registrado na base de dados.")); //Email não existente
+		}));
+	}
 }

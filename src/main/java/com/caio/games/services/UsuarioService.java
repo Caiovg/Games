f5a27f6a791a326c7600f538b949306eda4d1d36
unknown <caioviniciusg.1000@gmail.com>
2021-09-08 10:45:16 -0300

    Criação do metodo de findByID do usuario

--- src/main/java/com/caio/games/services/UsuarioService.java
+++ src/main/java/com/caio/games/services/UsuarioService.java
@@ -3,9 +3,11 @@ package com.caio.games.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.caio.games.services.exception.DataIntegratyViolationException;
+import com.caio.games.services.exception.ObjectNotFoundException;
 import com.caio.games.model.Usuario;
 import com.caio.games.repository.UsuarioRepository;
 
@@ -15,6 +17,7 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 
+	/*Busca todos aos usuarios*/
 	public List<Usuario> findAll(){
 		List<Usuario> list = repository.findAll();
 		if(list.isEmpty()) {
@@ -22,4 +25,13 @@ public class UsuarioService {
 		}
 		return list;
 	}
+	
+	/*
+	 * Busca pelo ID
+	 */
+	public ResponseEntity<Usuario> findById(Integer id) {
+		return repository.findById((int) id).map(
+				resp -> ResponseEntity.ok(resp)).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " não existe, Tipo: " + Usuario.class.getName()));
+	}
+	
 }

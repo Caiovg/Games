906d99160ced36eaad100fcc46f48c1d012ad1d2
unknown <caioviniciusg.1000@gmail.com>
2021-09-08 09:36:18 -0300

    Criação do metodo findAll usuarios

--- src/main/java/com/caio/games/services/UsuarioService.java
+++ src/main/java/com/caio/games/services/UsuarioService.java
@@ -1,8 +1,21 @@
 package com.caio.games.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.caio.games.model.Usuario;
+import com.caio.games.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
+	@Autowired
+	private UsuarioRepository repository;
+
+	public List<Usuario> findAll(){
+		List<Usuario> list = repository.findAll();
+		return list;
+	}
 }

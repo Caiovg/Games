d6e322e31364029b9cc01952348845b805112b78
unknown <caioviniciusg.1000@gmail.com>
2021-09-08 09:12:31 -0300

    Implementação dos metodos de UserDetailsImpl

--- src/main/java/com/caio/games/security/UserDetailsImpl.java
+++ src/main/java/com/caio/games/security/UserDetailsImpl.java
@@ -1,52 +1,68 @@
 package com.caio.games.security;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.caio.games.model.Usuario;
+
 public class UserDetailsImpl implements UserDetails{
 
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String password;
+	private List<GrantedAuthority> autorizacoes;
+	
+	public UserDetailsImpl(Usuario user) {
+		this.email = user.getEmail();
+		this.password = user.getSenha();
+	}
+	
+	public UserDetailsImpl() {}
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return autorizacoes;
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return email;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }

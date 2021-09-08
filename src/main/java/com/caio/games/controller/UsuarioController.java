dfdad589ca2e26b784bda0cc65f731326d1db2eb
unknown <caioviniciusg.1000@gmail.com>
2021-09-08 11:05:36 -0300

    Criação dos endpoints do usuario

--- src/main/java/com/caio/games/controller/UsuarioController.java
+++ src/main/java/com/caio/games/controller/UsuarioController.java
@@ -1,12 +1,31 @@
 package com.caio.games.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.caio.games.model.Usuario;
+import com.caio.games.services.UsuarioService;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
+
+	/*
+	 * Busca Todos
+	 */
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> obj = service.findAll();
+		return ResponseEntity.ok().body(obj);
+	}
 }
